b469ea22f761f660ba685a5d8d21d44daeabb8be
Mahmoudhamad837 <Mahmoudhamad837@gmail.com>
2026-06-24 15:01:33 +0300

    fixing environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'task-management-be-production-f356.up.railway.app',
+  apiUrl: 'https://task-management-be-production-f356.up.railway.app',
   authUrl: 'https://dummyjson.com',
 };
